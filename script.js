8788d9eea4d5bc673699320c09039680f8b0122c
Neha10 <nehabitla10@gmail.com>
2026-08-17 01:29:10 +0530

    Clear API_BASE URL in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://mental-health-score-backend.onrender.com";
+  const API_BASE = "";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
